e077df61b18a987da8ce87cdb184d79fd3c04b46
Leandro Silva <leandrosilvatj83jose@gmail.com>
2022-06-09 11:15:14 -0400

    Commit Inicial

---
+++ frames/untclient.pas
@@ -0,0 +1,44 @@
+﻿unit untclient;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Layouts, FMX.Objects, FMX.Effects, FMX.Controls.Presentation;
+
+type
+  TFrame1 = class(TFrame)
+    Layout1: TLayout;
+    Layout2: TLayout;
+    Circle1: TCircle;
+    ShadowEffect1: TShadowEffect;
+    labfoto: TLabel;
+    Layout3: TLayout;
+    Layout4: TLayout;
+    labnome: TLabel;
+    Layout5: TLayout;
+    Labcel: TLabel;
+    Layout6: TLayout;
+    FlowLayout1: TFlowLayout;
+    Layout7: TLayout;
+    imglist_ped: TLayout;
+    Layout8: TLayout;
+    Image1: TImage;
+    StyleBook1: TStyleBook;
+    Layout10: TLayout;
+    Circle2: TCircle;
+    labdetped: TLabel;
+    Layout11: TLayout;
+    imgaddped: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.fmx}
+
+end.
---
+++ myclients.dpr
@@ -0,0 +1,15 @@
+﻿program myclients;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  untprincipal in 'view\untprincipal.pas' {Form1},
+  untclient in 'frames\untclient.pas' {Frame1: TFrame};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
